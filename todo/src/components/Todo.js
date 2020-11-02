755c66cd20f0115efca95e8cd54687345bb8242c
Olivia Toolson <livytoolson@gmail.com>
2020-11-02 15:51:44 -0700

    clear completed working

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Todo = (props) => {
 
-    const eventListener = () => {
+    const handleToggle = () => {
         props.toggleItem(props.todo.id)
     }
 
     return (
         <div className="todo-list">
             <div className={`name${props.todo.completed ? ' completed' : ''}`}>
-                <p onClick={eventListener} className="list-item">{props.todo.item}</p>
+                <p onClick={handleToggle} className="list-item">{props.todo.item}</p>
             </div>
         </div>
     );
